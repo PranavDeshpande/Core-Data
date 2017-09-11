4eb1cd8ce221c9eb44415b5cc2f7e18c689ef6f8
Pranav Deshpande <pranavdeshpande12592@gmail.com>
2017-09-11 09:55:29 -0700

    commit

--- CData/ViewController.swift
+++ CData/ViewController.swift
@@ -9,15 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var data = [Array<Any>]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createData()
+        fetchData()
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func createData() {
+        // context is a request fr persistant container which has managed objects
+       let user = User(context: context)
+        
+        user.name = "Hi"
+        user.age = 20
+        
+        let user1 = User(context: context)
+        user1.name = "Hello"
+        user1.age = 10
+        appDelegate.saveContext()
+    }
+    
+    func fetchData() {
+        
+        do {
+            // we are saving the fetched info in a var called data with user.fetchrequest.. user bcoz user is the entity
+        try data = [context.fetch(User.fetchRequest())]
+            for each in data {
+                print(each)
+            }
+        }
+        catch {
+            print (error)
+        }
     }
 
 
